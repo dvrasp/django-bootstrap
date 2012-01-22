f081be5db38fd45051cf30833291b17da50d70db
David <david@reason-systems.com>
2012-01-22 20:02:38 +0100

    Renamed to django_bootstrap

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def fullsplit(path, result=None):
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-package_dir = "bootstrap"
+package_dir = "django_bootstrap"
 
 packages = []
 for dirpath, dirnames, filenames in os.walk(package_dir):
@@ -42,14 +42,14 @@ package_data = dict(
 )
 
 setup(
-    name = 'bootstrap',
+    name = 'django_bootstrap',
     description = 'Twitter Bootstrap ready Django forms.',
     long_description=read('README.mkd'),
     author='Earle Ady',
-    packages = ['bootstrap',],
+    packages = ['django_bootstrap',],
     package_data = package_data,
     version = '0.1.0',
-    url='http://github.com/earle/django-bootstrap',
+    url='http://github.com/dvrasp/django-bootstrap',
     keywords=[],
     license='Apache 2.0',
     classifiers=[
